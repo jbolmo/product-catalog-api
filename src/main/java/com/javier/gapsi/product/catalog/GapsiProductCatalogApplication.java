3f8b1dcd448d7556f5e440a508176c62813a0b92
jbolmo <jbolmo@gmail.com>
2026-04-16 19:57:19 -0500

    Initial proyect

---
+++ src/main/java/com/javier/gapsi/product/catalog/GapsiProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.javier.gapsi.product.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapsiProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GapsiProductCatalogApplication.class, args);
+	}
+
+}
